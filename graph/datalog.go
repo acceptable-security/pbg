2c9adfb8aa9cc931b26add75ed85e8e0b0673684
Avi Saven <block8437@gmail.com>
2020-03-02 21:24:22 -0500

    More work on TSV

--- graph/datalog.go
+++ graph/datalog.go
@@ -30,31 +30,17 @@ func getFilePred(filename string, pred string, files map[string] DestFile) *bufi
 }
 
 func (pbg *ProgramBehaviorGraph) GenerateDatalog(filename string) {
-	foundRels := make(map [string] bool)
-
-	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer file.Close()
-	writer := bufio.NewWriter(file)
-
 	ch := pbg.QueryTripletAsync("g.V().Tag('subject').Out(null, 'predicate').Tag('object').All()")
 
+	foundRels := make(map [string] bool)
 	files := make(map[string] DestFile)
 
 	for triplet := range ch {
 		pred := strings.ReplaceAll(triplet.predicate[1:len(triplet.predicate)-1], "-", "_")
 		writer := getFilePred(filename, pred, files)
-
-		if _, ok := foundRels[pred]; !ok {
-			foundRels[pred] = true;
-			writer.WriteString(fmt.Sprintf(".decl %s(from symbol, to symbol)\n", pred))
-		}
-
-		writer.WriteString(fmt.Sprintf("%s(%s, %s)\n", pred, triplet.subject, triplet.object))
+		subj := triplet.subject[1:len(subj)-1]
+		obj := triplet.object[1:len(obj)-1]
+		writer.WriteString(fmt.Sprintf("%s\t%s\n", subj, obj))
 	}
 
 	for pred, obj := range files {
